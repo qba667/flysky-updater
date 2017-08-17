e25dfd182c5935e57d93ac973695d70dd68f2893
qba667 <jklimasz@gmail.com>
2017-08-17 18:30:27 +0200

    Updater cosmetic implemented.

--- flysky_updater.go
+++ flysky_updater.go
@@ -6,6 +6,7 @@ import (
 	"encoding/hex"
 	"errors"
 	"fmt"
+	"path/filepath"
 	"github.com/cheggaaa/pb"
 	"github.com/tarm/serial"
 	"gopkg.in/alecthomas/kingpin.v2"
@@ -14,9 +15,9 @@ import (
 )
 
 var (
-	port     = kingpin.Arg("port", "port").Required().String()
-	filename = kingpin.Arg("filename", "filename").Required().String()
-	force    = kingpin.Flag("force", "Force flash.").Short('f').Bool()
+	//port     = kingpin.Arg("port", "port").Required().String()
+	//filename = kingpin.Arg("filename", "filename").Required().String()
+	//force    = kingpin.Flag("force", "Force flash.").Short('f').Bool()
 	verbose  = kingpin.Flag("verbose", "Verbose mode.").Short('v').Bool()
 )
 
@@ -167,7 +168,10 @@ func update(s *serial.Port, firmware []byte) error {
 			}
 		}
 		for chunk := 0; chunk < 1024; chunk += 256 {
-			err = write_chunk(s, start_address+bytes_written+chunk, firmware[bytes_written+chunk:bytes_written+chunk+256])
+			offset := bytes_written+chunk;
+			end := offset + 256;
+						
+			err = write_chunk(s, start_address+offset, firmware[offset:end])
 			if err != nil {
 				tries++
 				if tries <= 3 {
@@ -183,44 +187,148 @@ func update(s *serial.Port, firmware []byte) error {
 	return nil
 }
 
+
 func restart(s *serial.Port) error {
 	return WriteFrame(s, []byte{0xC1, 0x00})
 }
 
 func main() {
-	kingpin.Parse()
+	//kingpin.Parse()
 
-	data, err := ioutil.ReadFile(*filename)
+	fmt.Println(" _______ ___    __   __ _______ ___   _ __   __   ___ ___     ");
+	fmt.Println("|       |   |  |  | |  |       |   | | |  | |  | |   |   |    ");
+	fmt.Println("|    ___|   |  |  |_|  |  _____|   |_| |  |_|  | |   |   |___ ");
+	fmt.Println("|   |___|   |  |       | |_____|      _|       | |   |    _  |");
+	fmt.Println("|    ___|   |__|_     _|_____  |     |_|_     _| |   |   | | |");
+	fmt.Println("|   |   |       ||   |  _____| |    _  | |   |   |   |   |_| |");
+	fmt.Println("|___|   |_______||___| |_______|___| |_| |___|   |___|_______|");
+	fmt.Println("\r\n\r\nUpdater by mhils\r\nhttps://github.com/mhils/flysky-updater\r\n\r\n");
+	for {
+	binFiles, _ := filepath.Glob("./*.bin")
+	
+	if(len(binFiles) <=0) {
+		panic("No firmware found!!!")
+	}
+
+	var comports []int;
+	for p := 0; p < 24; p += 1 {
+		var portStr string = fmt.Sprintf("COM%d", p)
+		c := &serial.Config{Name: portStr, Baud: 115200, ReadTimeout: time.Second * 1}
+		s, err := serial.OpenPort(c)
+		if err != nil {
+			continue
+		}
+		s.Close()
+		comports = append(comports, p)
+		
+	}
+	if(len(comports) <=0) {
+		fmt.Println("No serial ports found!!!");
+		 time.Sleep(5000 * time.Millisecond);
+		continue;
+	}
+	var portIndex int
+	if(len(comports) == 1){
+		portIndex = comports[0];
+	} else{
+		for p := 0; p < len(comports); p += 1 {
+			fmt.Println(fmt.Sprintf("[%d] COM%d", comports[p], comports[p]));
+		}
+		fmt.Print("Please select serial port: ");
+		fmt.Scan(&portIndex)
+	}
+
+	fmt.Println(fmt.Sprintf("Selected serial port: COM %d", portIndex));
+	
+	var fwNames []string;
+	var firmwareFile int;
+	
+	
+	for f := 0; f < len(binFiles); f += 1 {
+		content, err := ioutil.ReadFile(binFiles[f])
+		if err != nil {
+			continue;
+		}
+		if(len(content) < 0xE700){
+			continue;
+		}
+		var fwAddress int = 0xD6AD;
+		var fwdate int = 0xD6C0;
+		if(len(content) > 0xE7FF){
+			fwAddress += 0x1800;
+			fwdate += 0x1800;
+		}
+		
+		fw := string(content[fwAddress:fwAddress+16]);
+		date := string(content[fwdate:fwdate+16]);
+		
+		fwNames = append(fwNames, fw +" " + date);
+		
+		fmt.Println(fmt.Sprintf("[%d] %s %d bytes %s %s", f, binFiles[f], len(content), fw, date));
+	}
+	if(len(binFiles) == 1){
+		firmwareFile = 0;
+	} else {
+		
+		fmt.Print("Please select firmware: ");
+		fmt.Scan(&firmwareFile)
+	}
+	
+	fmt.Println(fmt.Sprintf("Selected firmware: %s", fwNames[firmwareFile]));
+
+	raw, err := ioutil.ReadFile(binFiles[firmwareFile])
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		continue;
+	}
+	var data []byte;
+	
+	fmt.Println(fmt.Sprintf("File size: %d", len(raw)));
+	if len(raw) > 0xE7FF {
+        data = make([]byte, len(raw)-0x1800);
+		copy(data, raw[1800:len(data)]);
+    } else {
+        data = make([]byte, len(raw));
+		copy(data, raw[0:len(data)]);
+    }
+	
+	fmt.Println(fmt.Sprintf("Data size: %d", len(data)));
+	if (len(data) < 0x9000 || len(data) > 0xe7ff) {
+		fmt.Println(fmt.Sprintf("Unexpected firmare size: %d bytes", len(data)));
+		continue;
 	}
 
-	if (len(data) < 0x9000 || len(data) > 0xe7ff) && !*force {
-		panic(fmt.Sprintf("Unexpected firmare size: %d bytes", len(data)))
-	}
-
-	c := &serial.Config{Name: *port, Baud: 115200, ReadTimeout: time.Second * 1}
+	c := &serial.Config{Name: fmt.Sprintf("COM%d", portIndex), Baud: 115200, ReadTimeout: time.Second * 1}
 	s, err := serial.OpenPort(c)
 	if err != nil {
-		panic(err)
+		fmt.Println(err);
+		s.Close()
+		continue;
 	}
 
 	_, err = ping(s)
 	if err != nil {
-		panic(err)
+		fmt.Println(err);
+		s.Close()
+		continue;
 	}
 
 	err = update(s, data)
 	if err != nil {
-		panic(err)
+		fmt.Println(err);
+		s.Close()
+		continue;
 	}
 
 	err = restart(s)
 	if err != nil {
-		panic(err)
+		fmt.Println(err);
+		s.Close()
+		continue;
 	}
-
-	fmt.Println("Success!")
-
 	s.Close()
+	break;
+	}
+	fmt.Println("Success!")
+	
 }
